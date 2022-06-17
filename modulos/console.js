efb9d8fdf588c09aeb9d4e2f852c0290375d69ef
jzlbornoz <javieraap10@gmail.com>
2022-06-16 22:01:30 -0400

    [console]

---
+++ modulos/console.js
@@ -0,0 +1,27 @@
+const table = [
+    {
+        12:"alfre",
+        34: "Wilm",
+        22:"as",
+        54: "wio",
+        
+    },
+    {
+        122:"alfr12e",
+        342: "Wi124lm",
+        222:"a23s",
+        524: "w23io",
+        
+    }
+]
+
+
+console.table(table);
+
+console.group("Talking");
+console.log("Hi");
+console.log("Hi");
+console.log("bla bla bla");
+console.log("bye");
+console.groupEnd("Talking");
+console.log("otra monda")
